90210c808525a0286894707f3607fe85c76c17b3
Jian Ma <jianquanma@gmail.com>
2024-08-27 22:36:28 -0400

    Initial Commit

---
+++ AirVetSwiftUI/AirVetSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirVetSwiftUIApp.swift
+//  AirVetSwiftUI
+//
+//  Created by Jian Ma on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct AirVetSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
