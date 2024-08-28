c67a3eb0628e006c4b4e6a4d6885fdf5fd33f3a7
Jian Ma <jianquanma@gmail.com>
2024-08-28 00:24:06 -0400

    added save functionality

--- AirVetSwiftUI/TaskListViewModel.swift
+++ AirVetSwiftUI/TaskListViewModel.swift
@@ -4,11 +4,21 @@
 //
 //  Created by Jian Ma on 8/27/24.
 //
-
+import Foundation
 import Combine
 
 class TaskListViewModel: ObservableObject {
-    @Published var tasks: [Task] = []
+    @Published var tasks: [Task] = [] {
+        didSet {
+            saveTasks()
+        }
+    }
+
+    private let tasksKey = "tasksKey"
+
+    init() {
+        loadTasks()
+    }
 
     func addTask(name: String, description: String) {
         let newTask = Task(name: name, description: description)
@@ -20,4 +30,18 @@ class TaskListViewModel: ObservableObject {
             tasks[index].isCompleted.toggle()
         }
     }
+
+    private func saveTasks() {
+        if let encodedTasks = try? JSONEncoder().encode(tasks) {
+            UserDefaults.standard.set(encodedTasks, forKey: tasksKey)
+        }
+    }
+
+    private func loadTasks() {
+        if let savedTasksData = UserDefaults.standard.data(forKey: tasksKey),
+           let decodedTasks = try? JSONDecoder().decode([Task].self, from: savedTasksData) {
+            tasks = decodedTasks
+        }
+    }
 }
+
--- AirVetSwiftUI/TaskModel.swift
+++ AirVetSwiftUI/TaskModel.swift
@@ -7,9 +7,16 @@
 
 import Foundation
 
-struct Task: Identifiable {
-    let id = UUID()
+struct Task: Identifiable, Codable {
+    let id: UUID
     var name: String
     var description: String
-    var isCompleted: Bool = false
+    var isCompleted: Bool
+
+    init(id: UUID = UUID(), name: String, description: String, isCompleted: Bool = false) {
+        self.id = id
+        self.name = name
+        self.description = description
+        self.isCompleted = isCompleted
+    }
 }
